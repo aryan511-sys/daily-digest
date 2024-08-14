8e95fdbe4b0f69b4d1d64f72e6f1fb51491cc350
aryan511-sys <gajerarajesh4317@gmail.com>
2024-08-14 18:26:31 +0530

    UseEffect React Hooks

--- src/Components/ReactHook1.jsx
+++ src/Components/ReactHook1.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
 
 function ReactHook1() {
 
     const [count, setCount] = useState(0);
+    useEffect(() => {
+        // Update the document title using the browser API
+        document.title = `You clicked ${count} times`;
+      });
     
+
     return (
         <div className='text-center'>
             <p>You clicked {count} times</p>
